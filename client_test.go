fbb553afeae02e275e910b86a92d874fc9a0a420
Mike Lloyd <mlloyd@pivotal.io>
2017-11-13 15:54:35 -0500

    Added GET to region functionality.
    Don't use the GetRegion API, please, haha.
    
    Signed-off-by: Mike Lloyd <mlloyd@pivotal.io>

--- client_test.go
+++ client_test.go
@@ -1,6 +1,7 @@
 package gofire
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 
@@ -9,6 +10,24 @@ import (
 
 var testTargetGeode string
 var geodeTestClient *Client
+
+type testRegion struct {
+	// the struct tag has to match the region name or this will fail.
+	Region interface{} `json:"testRegion"`
+}
+
+type testData struct {
+	Message string `json:"message"`
+	Value   int    `json:"value"`
+}
+
+type testNestedData struct {
+	Message string `json:"message"`
+	Object struct {
+		Message string `json:"message"`
+		Value   int    `json:"value"`
+	} `json:"object"`
+}
 
 func TestMain(m *testing.M) {
 	testTargetGeode = os.Getenv("GEODE_URL")
@@ -36,6 +55,15 @@ func TestClient_GetServers(t *testing.T) {
 
 func TestClient_GetRegions(t *testing.T) {
 	regions, err := geodeTestClient.GetRegions()
-	assert.Nil(t, err, )
+	assert.Nil(t, err)
 	assert.EqualValues(t, regions.RegionInfo[0].Name, "testRegion")
+}
+
+func TestClient_GetRegion(t *testing.T) {
+	data, err := geodeTestClient.GetRegion("testRegion", 50, false)
+	assert.Nil(t, err, "failed to get data from testRegion.")
+	var tmpData testRegion
+	if err := json.Unmarshal(data, &tmpData); err != nil {
+		assert.Nil(t, err, "cannot unmarshal json data.")
+	}
 }
--- region.go
+++ region.go
@@ -1,11 +1,14 @@
 package gofire
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"io/ioutil"
+)
 
 type RegionInfo struct {
-	Name string `json:"name"`
-	RegionType string `json:"type"`
-	KeyConstraint string `json:"key-constraint"`
+	Name            string `json:"name"`
+	RegionType      string `json:"type"`
+	KeyConstraint   string `json:"key-constraint"`
 	ValueConstraint string `json:"value-constraint"`
 }
 
@@ -13,6 +16,7 @@ type Regions struct {
 	RegionInfo []RegionInfo `json:"regions"`
 }
 
+// Lists out the regions a given server hosts.
 func (cl Client) GetRegions() (Regions, error) {
 	regions := Regions{
 		RegionInfo: make([]RegionInfo, 0),
@@ -25,8 +29,31 @@ func (cl Client) GetRegions() (Regions, error) {
 	if err != nil {
 		return regions, err
 	}
+	defer resp.Body.Close()
+
 	if err := json.NewDecoder(resp.Body).Decode(&regions); err != nil {
 		return regions, err
 	}
 	return regions, nil
 }
+
+// Read data for the region. This is not a good API for golang since it's purely dynamic. If you want to use it, you have
+// to decode it, heh. Author's recommendation: don't use this API.
+func (cl Client) GetRegion(region string, limit int, all bool) ([]byte, error) {
+	req, err := cl.getRequestBuilder("/" + region)
+	if err != nil {
+		return []byte(""), nil
+	}
+	resp, err := cl.intClient.Do(req)
+	if err != nil {
+		return []byte(""), err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []byte(""), err
+	}
+	return body, nil
+}
+
